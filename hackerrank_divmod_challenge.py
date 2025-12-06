66e90bf87627a3a4e6203c535f4801ce1d8dc977
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2025-12-06 06:34:16 +0200

    Solved Hackerrank divmod() challenge: implemented integer division, modulus, and tuple output using Python's built-in divmod().

---
+++ hackerrank_divmod_challenge.py
@@ -0,0 +1,8 @@
+# divmod() Hackerrank challenge solution
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print("Integer division:", a // b)
+print("Remainder:", a % b)
+print("Divmod result:", divmod(a, b))

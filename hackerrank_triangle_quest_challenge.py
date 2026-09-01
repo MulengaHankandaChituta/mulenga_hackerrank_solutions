2dd8b8ea84e44eabb18f8c3ea2b502c31dc63ff1
mulenga <mulengahankandachituta@gmail.com>
2026-09-01 07:08:14 +0200

    Solve HackerRank numerical triangle challenge using arithmetic operations

--- hackerrank_triangle_quest_challenge.py
+++ hackerrank_triangle_quest_challenge.py
@@ -1,7 +1,4 @@
-# This program prints a palindromic number triangle of size n.
-# It uses only mathematics (no strings), as required by the HackerRank challenge.
-# The expression ((10**i - 1) // 9) generates a number made of i ones (1, 11, 111...).
-# Squaring that number creates the palindromic pattern (1, 121, 12321...).
+n = int(input())
 
-for i in range(1, int(input("Enter size: ")) + 1):
-    print(((10**i - 1) // 9) ** 2)
+for i in range(1, n):
+    print((10**i // 9) * i)
